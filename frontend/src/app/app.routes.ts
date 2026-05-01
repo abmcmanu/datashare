71fcac68de08dd8588d5362b7ceebafd0a241a32
abmcmanu <manueolinga@gmail.com>
2026-05-01 10:30:47 +0200

    fix(routes) fix route path error

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -21,7 +21,7 @@ export const APP_ROUTES: Routes = [
       import('./features/auth/signup/signup.component').then((m) => m.SignupComponent)
   },
   {
-    path: 'd',
+    path: 'dashboard',
     loadComponent: () =>
       import('./features/dashboard/dashboard.component').then((m) => m.DashboardComponent)
   },
